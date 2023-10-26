f9fe18b50e27917ab91e2d99687aba0b1fa285da
yorranan <yorranan.costa@outlook.com>
2023-10-26 11:27:33 -0300

    movie-details

--- src/app/view/movie-details/movie-details.page.ts
+++ src/app/view/movie-details/movie-details.page.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { OmdbapiService } from 'src/app/services/omdbapi.service';
 
 @Component({
   selector: 'app-movie-details',
@@ -7,9 +9,13 @@ import { Component, OnInit } from '@angular/core';
 })
 export class MovieDetailsPage implements OnInit {
 
-  constructor() { }
+  info: any
+
+  constructor(private actRoute: ActivatedRoute, private omdbapi: OmdbapiService) { }
 
   ngOnInit() {
+    let id = this.actRoute.snapshot.paramMap.get('id')
+    this.omdbapi.geById(id).subscribe(result => this.info = result)
   }
 
 }
