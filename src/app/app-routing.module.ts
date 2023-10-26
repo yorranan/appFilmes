4434f9daebc9c7ef239d2a73a36314b2b31f64af
yorranan <yorranan.costa@outlook.com>
2023-10-26 10:45:52 -0300

    first commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,13 +3,17 @@ import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
 
 const routes: Routes = [
   {
-    path: 'home',
-    loadChildren: () => import('./home/home.module').then( m => m.HomePageModule)
+    path: '',
+    redirectTo: 'movies',
+    pathMatch: 'full'
   },
   {
-    path: '',
-    redirectTo: 'home',
-    pathMatch: 'full'
+    path: 'movies',
+    loadChildren: () => import('./view/movies/movies.module').then( m => m.MoviesPageModule)
+  },
+  {
+    path: 'movie-details/:id',
+    loadChildren: () => import('./view/movie-details/movie-details.module').then( m => m.MovieDetailsPageModule)
   },
 ];
 
