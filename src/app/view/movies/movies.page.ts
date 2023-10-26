e0d3f39f4ba3165c0fcc0939269658dade4f4b54
yorranan <yorranan.costa@outlook.com>
2023-10-26 11:27:44 -0300

    movies-page

--- src/app/view/movies/movies.page.ts
+++ src/app/view/movies/movies.page.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Observable, map } from 'rxjs';
+import { OmdbapiService, SearchType } from 'src/app/services/omdbapi.service';
 
 @Component({
   selector: 'app-movies',
@@ -7,9 +9,17 @@ import { Component, OnInit } from '@angular/core';
 })
 export class MoviesPage implements OnInit {
 
-  constructor() { }
+  result!: Observable<any>
+  searchTerms: string = ''
+  type: SearchType = SearchType.all
+
+  constructor(private omdbapi: OmdbapiService) { }
 
   ngOnInit() {
+  }
+
+  search() {
+    this.result = this.omdbapi.getAll(this.searchTerms, this.type).pipe(map(results => results['Search']))
   }
 
 }
