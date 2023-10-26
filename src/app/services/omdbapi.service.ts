1710957f02ac3317f2835dcf18590551e977ffa9
yorranan <yorranan.costa@outlook.com>
2023-10-26 11:28:36 -0300

    api connect

--- src/app/services/omdbapi.service.ts
+++ src/app/services/omdbapi.service.ts
@@ -1,9 +1,30 @@
-import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Injectable, getNgModuleById } from '@angular/core';
+import { Observable } from 'rxjs';
+import { environment } from 'src/environments/environment';
+
+export enum SearchType{
+  all = '',
+  movie = 'movie',
+  series = 'series',
+  episode = 'episode'
+}
 
 @Injectable({
   providedIn: 'root'
 })
 export class OmdbapiService {
+  url = 'http://www.omdbapi.com/'
+  key = environment.apiKey;
+  
+  constructor(private http: HttpClient) {}
 
-  constructor() { }
+  getAll(title: string, type: SearchType)
+  : Observable<any>{
+    return this.http.get(`${this.url}?s=${encodeURI(title)}&type=${type}&apiKey=${this.key}`)
+  }
+
+  geById(id: any){
+    return this.http.get(`${this.url}?i=${id}&plot=full&apiKey=${this.key}`)
+  }
 }
